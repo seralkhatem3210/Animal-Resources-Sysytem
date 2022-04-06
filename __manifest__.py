861b447492cf21f92f6f1e78d563f22941ce4382
seralkhatem3210 <57324576+seralkhatem3210@users.noreply.github.com>
2022-04-07 00:03:56 +0300

    asd

--- __manifest__.py
+++ __manifest__.py
@@ -1,7 +1,7 @@
 {
 	'name': "Animal Resource System",
 	'sequence': -100,
-	'author': "Sirelkhatim Gamal Sirelkhatim",
+	'author': "Sirelkhatim Gamal Sirelkhatim Musa",
 	'summary': "Agriclutre Odoo System",
 	'description' : """ 
 		Project  Odoo:
